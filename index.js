2da19135bf4ed2985d906b50dfc8991641447a05
Tech-goodat <felixkiprotich2000@gmail.com>
2023-09-19 09:09:10 +0300

    done halfway with the assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Write your code in this file!
+const currentUser='Felix Kiprotich';
+const welcomeMessage='Welcome to Flatbook, Felix Kiprotich! ';
+const excitedWelcomeMessage='${welcomeMessage}.toUpprecase(), ${currentUser}!'
